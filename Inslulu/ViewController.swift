1172bcf091cad70443f1e407e83d1e053b7b6bcb
Albert <fangmengkai@xinghuo.us>
2025-05-20 11:16:16 +0800

    Initial Commit

---
+++ Inslulu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Inslulu
+//
+//  Created by insnap on 2025/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
